cfd127bb6c34e41b7c56c7bd0e3b27733019e351
Lúcás Críostóir Meier <cronokirby@gmail.com>
2022-01-29 12:32:32 +0100

    Implement an echo prompt

--- src/main.c
+++ src/main.c
@@ -1,5 +1,24 @@
+#include "stdint.h"
 #include "stdio.h"
 
+/// The number of bytes in our line buffer.
+const size_t LINE_BUFFER_SIZE = (1 << 14);
+
+// The prompt to display in the shell.
+const char *PROMPT = ">> ";
+
 int main() {
-  puts("Hello World!");
+  char line_buffer[LINE_BUFFER_SIZE];
+
+  for (;;) {
+    fputs(PROMPT, stdout);
+    if (fgets(line_buffer, LINE_BUFFER_SIZE, stdin) == NULL) {
+      if (feof(stdin)) {
+        break;
+      }
+      perror("Error reading line:");
+      continue;
+    }
+    fputs(line_buffer, stdout);
+  }
 }
